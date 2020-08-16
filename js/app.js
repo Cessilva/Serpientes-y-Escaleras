9d0c378695b3a2db3c32c8ebdfb2a893e23b307a
Cessilva <cecilia.proteco@gmail.com>
2020-08-16 13:19:28 -0500

    serpientes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var posEqu4=0;
 var textoglobal;
 var correcto;
 var incorrecto;
+var equipo_elegido;
 
 function rollDice(){
     var die1 = document.getElementById("die1");
@@ -20,25 +21,37 @@ function rollDice(){
       posEqu1= posEqu1+d1;
       elemento.innerHTML= elemento.innerHTML+"Equipo 1, Posicion:"+posEqu1;
       azul='azul';
+      equipo_elegido=1;
+      posEqu1=serpiente(posEqu1)
       navegacion(azul,posEqu1)
+      
     }else if (posicion.innerHTML == "equipo2"){
       elemento.innerHTML = textoglobal[posEqu2];
       posEqu2= posEqu2+d1;
       elemento.innerHTML=elemento.innerHTML+"Equipo 2, Posicion:"+posEqu2;
       rojo='rojo';
+      equipo_elegido=2;
+      posEqu2=serpiente(posEqu2)
       navegacion(rojo,posEqu2)
+      
     }else if (posicion.innerHTML == "equipo3"){
       elemento.innerHTML = textoglobal[posEqu3];
       posEqu3= posEqu3+d1;
       elemento.innerHTML=elemento.innerHTML+"Equipo 3, Posicion:"+posEqu3;
       verde='verde';
+      equipo_elegido=3;
+      posEqu3=serpiente(posEqu3)
       navegacion(verde,posEqu3)
+      
     }else {
       elemento.innerHTML = textoglobal[posEqu4];
       posEqu4= posEqu4+d1;
       elemento.innerHTML=elemento.innerHTML+"Equipo 4, Posicion:"+posEqu4;
       amarillo='amarillo';
+      equipo_elegido=4;
+      posEqu4=serpiente(posEqu4)
       navegacion(amarillo,posEqu4)
+      
     }
     if (posEqu1 > 99 || posEqu2 > 99 || posEqu3 > 99 || posEqu4 > 99){
     	alert("EL juego ha terminado, gano el equipo:");
@@ -74,10 +87,9 @@ function navegacion(equipo,posicion){
   var marginLeft=12;
   var marginTop=-52;
   var lado=52.5;
-  var derecha=1;
-  var izquierda=0;
   var cambio=0
-  // De lado a lado
+
+  // Derecha a izquierda
   if ((posicion>0 && posicion<=10)||(posicion>20 && posicion<=30)||(posicion>40 && posicion<=50)||(posicion>60 && posicion<=70)||(posicion>80 && posicion<=90)){
     if(posicion<=10){
       cambio2=parseInt(String(posicion).substring(0, 1));
@@ -98,20 +110,55 @@ function navegacion(equipo,posicion){
       document.getElementById(equipo).style.marginLeft= String(lado*cambio2+marginLeft)+"px";
     }
   }
-  //Para subir o bajar 
-  if (posicion<=10){
+  //Subir o bajar
+  if (posicion<=9){
     cambio=0;
     document.getElementById(equipo).style.marginTop= String(marginTop-(lado*cambio))+"px";
   }else{
     cambio=parseInt(String(posicion).substring(0, 1));
     document.getElementById(equipo).style.marginTop= String(marginTop-(lado*cambio))+"px";
   }
+
+
 }
+
+
 function esCorrecto(){
   correcto=1;
   incorrecto=0;
 }
+
 function esIncorrecto(){
   correcto=0;
   incorrecto=1;
+}
+
+function serpiente(posicion){
+  switch (posicion) {
+    case 25:
+      posicion=3;
+      break;
+    case 35:
+      posicion=9;
+      break;
+    case 45:
+      posicion=21;
+      break;
+    case 48:
+      posicion=12;
+    break;
+    case 78:
+      posicion=42;
+      break;
+    case 87:
+      posicion=46;
+      break;
+    case 94:
+      posicion=57;
+      break;
+    default:
+      posicion=posicion;
+      break;
+  }
+  return posicion;
 }
